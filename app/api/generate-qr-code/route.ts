5b5797e2e05bda409a7b3a56a56ab506feae0a32
ashfaqjani916 <ashfaqjani916@gmail.com>
2025-06-27 22:54:00 +0530

    Refactor generate-qr-code endpoint to return a single QR image

--- app/api/generate-qr-code/route.ts
+++ app/api/generate-qr-code/route.ts
@@ -1,21 +1,26 @@
-
-
-
-
 import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
+import QrCode from 'qrcode';
 
 const prisma = new PrismaClient();
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'; // Replace with your production URL
 
 export async function GET() {
   try {
-    const qrCodes = await prisma.qrCode.findMany();
-    const qrCodeUrls = qrCodes.map((qrCode: { id: string }) => `${baseUrl}/ingredient/${qrCode.id}`);
+    const qrCode = await prisma.qrCode.findFirst(
+      {
+        where: { usedByUser: null }
+      }
+    );
 
-    return NextResponse.json({
-      qrCodeUrls: qrCodeUrls
-    }, { status: 200 });
+    const qrCodeUrl = `${baseUrl}/ingredients/${qrCode?.id}`
+    const QRpng = await QrCode.toBuffer(qrCodeUrl, { type: 'png' });
+
+    return new NextResponse(new Uint8Array(QRpng), {
+      headers: {
+        'Content-type': 'image/png'
+      }
+    });
   } catch (error) {
     console.error('Error generating QR code URLs:', error);
     return NextResponse.json({ error: 'Failed to generate QR code URLs' }, { status: 500 });
